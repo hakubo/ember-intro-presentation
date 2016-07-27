12d4abdbf6b18cb751aa440516e861b129e8d5c4
Jakub Olek <bukaj.kelo+github@gmail.com>
2016-07-27 21:08:08 +0200

    add ember-cli-deploy

---
+++ config/deploy.js
@@ -0,0 +1,46 @@
+/* jshint node: true */
+
+module.exports = function(deployTarget) {
+  var ENV = {
+    build: {}
+    // include other plugin configuration that applies to all deploy targets here
+  };
+
+  ENV.s3 = {
+    accessKeyId: process.env.ACCESS_KEY_ID,
+    secretAccessKey: process.env.SECRET_ACCESS_KEY,
+    bucket: process.env.BUCKET,
+    region: process.env.REGION
+  };
+
+  ENV['s3-index'] = {
+    accessKeyId: process.env.ACCESS_KEY_ID,
+    secretAccessKey: process.env.SECRET_ACCESS_KEY,
+    bucket: process.env.BUCKET,
+    region: process.env.REGION
+  };
+
+  ENV.pipeline = {
+    activateOnDeploy: true
+  };
+
+  if (deployTarget === 'development') {
+    ENV.build.environment = 'development';
+    // configure other plugins for development deploy target here
+  }
+
+  if (deployTarget === 'staging') {
+    ENV.build.environment = 'production';
+    // configure other plugins for staging deploy target here
+  }
+
+  if (deployTarget === 'production') {
+    ENV.build.environment = 'production';
+    // configure other plugins for production deploy target here
+  }
+
+  // Note: if you need to build some configuration asynchronously, you can return
+  // a promise that resolves with the ENV object instead of returning the
+  // ENV object synchronously.
+  return ENV;
+};
